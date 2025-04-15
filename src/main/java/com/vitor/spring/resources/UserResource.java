237b5cf53e494b0818f49bad7c9ecc04b27fd15f
vitormotaleite <vitor.mota.diniz@ccc.ufcg.edu.br>
2025-04-15 15:14:31 -0300

    Camada de serviços e registro de componentes

--- src/main/java/com/vitor/spring/resources/UserResource.java
+++ src/main/java/com/vitor/spring/resources/UserResource.java
@@ -1,20 +1,33 @@
 package com.vitor.spring.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vitor.spring.entities.User;
+import com.vitor.spring.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L, "maria", "maria@gmail", "999", "123");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = userService.findAll();
+        return ResponseEntity.ok().body(list);
     }
-    
+
+   @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = userService.findUserById(id);
+        return ResponseEntity.ok().body(obj);
+    }
 }
