91b1d5f97299593336bf04de55197ef6848a2f39
vitormotaleite <vitor.mota.diniz@ccc.ufcg.edu.br>
2025-04-18 10:46:13 -0300

    metodos total e subtotal

--- src/main/java/com/vitor/spring/entities/Payment.java
+++ src/main/java/com/vitor/spring/entities/Payment.java
@@ -3,6 +3,8 @@ package com.vitor.spring.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,6 +24,7 @@ public class Payment implements Serializable {
 
     @OneToOne
     @MapsId
+    @JsonIgnore
     private Order order;
 
     public Payment() {
